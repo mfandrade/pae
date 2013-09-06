c70b4064e7da005569490041e44b57a8cf2fec06
Marcelo F Andrade <mfandrade@gmail.com>
2013-09-06 15:54:40 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pae::Application.config.secret_key_base = 'a842c70cfacb6151499a261b27edab6c8e725e4f77336a59cbf465524a7278979532ea6b825347519624935d19d84fbd8f469e2817567315d013c64676fde89d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Pae::Application.config.session_store :cookie_store, key: '_pae_session'
